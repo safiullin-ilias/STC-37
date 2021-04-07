a076012d165d69d253b7694a2d5d39ba11ff6c03
safiullin il'yas <safiullinir@live.ru>
2021-04-07 11:31:32 +0300

    part3.lesson05.task2

--- HW_9/Main.java
+++ HW_9/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         NumberProcess NP = new NumberProcess();
         StringProcess SP = new StringProcess();
+        NumberAndStringProcessor NaSP = new NumberAndStringProcessor(new int [] {5051, 7, 55, 135, 7842}, new String[] {"tut", "byil", "Ilias"});
+
+
         System.out.println(NP.REVERSE(51597));
         System.out.println(NP.DELETE_NULLS(301052500));
         System.out.println(NP.NE4ETTOLOW(165123165));
@@ -12,6 +15,8 @@ public class Main {
         System.out.println(SP.DELETE_DIGITS("On rodilsya v 1994"));
         System.out.println(SP.LOW_TO_UPPER("LLltAkeTnYI nabor symbolov"));
 
+        System.out.println(java.util.Arrays.toString(NaSP.REVERSE_INTS()));
+        System.out.println(java.util.Arrays.toString(NaSP.REVERSE_STRINGS()));
         
     }
 }
--- HW_9/NumberAndStringProcessor.java
+++ HW_9/NumberAndStringProcessor.java
@@ -1,5 +1,56 @@
 package HW_9;
 
 public class NumberAndStringProcessor {
+    String[] Strings;
+    int[] Numbers;
+
+    public NumberAndStringProcessor(String[] strings) {
+        Strings = strings;
+        Numbers = new int[0];
+    }
+
+    public NumberAndStringProcessor(int[] numbers) {
+        Strings = new String[0];
+        Numbers = numbers;
+    }
     
+    public NumberAndStringProcessor(int[] numbers, String[] strings) {
+        Strings = strings;
+        Numbers = numbers;
+    }
+
+    public int[] REVERSE_INTS() {
+        return process_ints((numb) -> {
+            int new_number = 0;
+            while (numb > 0) {
+                int t = numb % 10;
+                numb /= 10;
+                new_number = new_number * 10 + t;
+            }
+            return new_number;
+        });
+    }
+
+    public String[] REVERSE_STRINGS() {
+        return process_strings((str) -> {
+            String new_string = "";
+            for (int i = str.length() - 1; i >= 0; --i)
+                new_string += str.charAt(i);
+            return new_string;
+        });
+    }
+
+    public int[] process_ints(INumberProcess function) {
+        int[] data = new int[Numbers.length];
+        for (int i = 0; i< Numbers.length; i++)
+            data[i] = function.process(Numbers[i]);
+        return data;
+    }
+
+    public String[] process_strings(IStringProcess function) {
+        String[] data = new String[Strings.length];
+        for (int i = 0; i< Strings.length; i++)
+            data[i] = function.process(Strings[i]);
+        return data;
+    }
 }
